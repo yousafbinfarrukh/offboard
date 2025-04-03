6e51f46f6a5cb6b6ac5d4b071cf2a4c8cd7713bf
Yousaf Bin Farrukh <yousasfbinfarrukh@gmail.com>
2025-04-03 21:59:06 +0500

    Added control station node

---
+++ src/control_station.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+from std_msgs.msg import Bool
+from geometry_msgs.msg import PoseStamped
+import rospy
+from curtsies import Input
+
+
+
+class Scheduler:
+    def __init__(self):
+        rospy.init_node("Control_Station", anonymous=False)
+        self.takeoffPublisher               = rospy.Publisher("/takeOff", Bool, queue_size=2)
+        self.offboardPublisher              = rospy.Publisher("/offboard", Bool, queue_size=2)
+        self.landPublisher                  = rospy.Publisher("/landing", Bool, queue_size=2)
+        self.positionPublisher              = rospy.Publisher("/target_position", PoseStamped, queue_size=2)
+
+    def takeoff(self):
+        rate = rospy.Rate(10)
+        takeoff = Bool()
+        land = Bool()
+        takeoff = True
+        land = False
+        
+        for i in range(0,10):
+            self.landPublisher.publish(land)
+            rate.sleep()
+
+        for i in range(0,10):
+            self.takeoffPublisher.publish(takeoff)
+            self.offboardPublisher.publish(takeoff)
+            rate.sleep()
+
+        rospy.sleep(10)
+
+
+    def land(self):
+        rate = rospy.Rate(10)
+        takeoff = Bool()
+        land = Bool()
+        takeoff = False
+        land = True
+        
+        for i in range(0,10):
+            self.takeoffPublisher.publish(takeoff)
+            self.offboardPublisher.publish(takeoff)
+            self.landPublisher.publish(land)
+            rate.sleep()
+
+
+    def scan(self):
+        rate = rospy.Rate(10)
+        position = PoseStamped()
+        position.pose.position.x = 0
+        position.pose.position.y = 0
+        position.pose.position.z = 25
+        position.pose.orientation.x = 0
+        position.pose.orientation.y = 0
+        position.pose.orientation.z = 1
+        position.pose.orientation.w = 0
+        
+        for i in range(0,10):
+            self.positionPublisher.publish(position)
+            rate.sleep()
+
+        
+
+def main():
+    uav = Scheduler()
+    key=0
+    key_takeoff         = "'<Ctrl-t>'"
+    key_land            = "'<Ctrl-l>'"
+    key_scan            = "'<Ctrl-a>'"
+    key_break           = "'b'"
+    print("Control Station")
+
+
+    with Input(keynames='curtsies') as input_generator:
+        for i in input_generator:
+            msg  = [0] * 15
+            key     = repr(i)
+            print(key)
+            if key == key_takeoff or key_land or key_scan or key == key_break:
+                if  key==key_takeoff:
+                    print("takeoff")
+                    uav.takeoff()
+                if  key==key_land:
+                    print("Landing")
+                    uav.land()
+                if  key==key_scan:
+                    print("Scanning")
+                    uav.scan()
+                if key==key_break:
+                    break
+        
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
